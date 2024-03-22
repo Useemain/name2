aaa288389b0dfd218c40013a74fe84737d894ccf
Usee <yasno.ponyatno229@gmail.com>
2024-03-22 16:32:46 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void main(String[] args) {
+        int startBalance = 100;
+        int added = 1500;
 
+        int bonus = added > 1000 ? added / 100 : 0;
+        int finalBalance = startBalance + added + bonus;
+        System.out.println("Итоговый счет :" + finalBalance);
+        System.out.println("Бонус :" + bonus);
+    }
 }
